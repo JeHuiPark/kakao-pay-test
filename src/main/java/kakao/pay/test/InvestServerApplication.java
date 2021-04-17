604e96ab7b1b865a24ab5261dc0dd480f14a4f52
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-17 15:21:21 +0900

    메인 프로그램을 정의하라

---
+++ src/main/java/kakao/pay/test/InvestServerApplication.java
@@ -0,0 +1,12 @@
+package kakao.pay.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InvestServerApplication.class, args);
+  }
+}
