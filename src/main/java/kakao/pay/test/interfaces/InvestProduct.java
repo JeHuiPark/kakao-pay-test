7acb3377bc596ce87dc5e2a174a8441f69fab23b
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-17 16:18:24 +0900

    투자 상품을 정의하라

---
+++ src/main/java/kakao/pay/test/interfaces/InvestProduct.java
@@ -0,0 +1,32 @@
+package kakao.pay.test.interfaces;
+
+/**
+ * 투자상품.
+ */
+public interface InvestProduct {
+
+  /**
+   * 상품 ID.
+   */
+  long productId();
+
+  /**
+   * 상품 제목.
+   */
+  String title();
+
+  /**
+   * 총 모집금액.
+   */
+  long totalInvestingAmount();
+
+  /**
+   * 모집기간.
+   */
+  InvestPeriod investPeriod();
+
+  /**
+   * 투자상품 종류.
+   */
+  InvestProductType investProductType();
+}
