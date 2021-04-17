24394990394d8d30ebc88218f9881ae6a1cbe0a8
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-18 01:17:52 +0900

    ProductInvestorLogger 인터페이스를 제거하라

--- src/main/java/kakao/pay/test/invest/impl/jpa/ProductInvestmentLogRepository.java
+++ src/main/java/kakao/pay/test/invest/impl/jpa/ProductInvestmentLogRepository.java
@@ -1,16 +1,18 @@
 package kakao.pay.test.invest.impl.jpa;
 
 import java.util.Optional;
-import kakao.pay.test.invest.impl.ProductInvestorLogger;
 import kakao.pay.test.invest.interfaces.ProductInvestor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface ProductInvestmentLogRepository extends JpaRepository<ProductInvestmentLog, Long>,
-    ProductInvestorLogger {
+interface ProductInvestmentLogRepository extends JpaRepository<ProductInvestmentLog, Long> {
 
   Optional<ProductInvestmentLog> findByKey(String key);
 
-  @Override
+  /**
+   * 상품 투자자 정보를 기록 합니다.
+   * @param productInvestor 기록 대상
+   * @return 기록된 적이 없으면 true 를 리턴합니다.
+   */
   default boolean logging(ProductInvestor productInvestor) {
     var productInvestorLog = new ProductInvestmentLog(productInvestor);
     if (findByKey(productInvestorLog.getKey()).isEmpty()) {
