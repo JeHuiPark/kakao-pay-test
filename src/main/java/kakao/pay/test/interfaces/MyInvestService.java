69f61314be50c36a4dc934212420113056dc5ba3
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-17 16:41:48 +0900

    나의 투자정보 서비스를 정의하라

---
+++ src/main/java/kakao/pay/test/interfaces/MyInvestService.java
@@ -0,0 +1,16 @@
+package kakao.pay.test.interfaces;
+
+import java.util.List;
+
+/**
+ * 나의 투자정보 서비스.
+ */
+public interface MyInvestService {
+
+  /**
+   * 나의 투자상품목록을 리턴합니다.
+   * @param userId 사용자 식별값
+   * @return 나의 투자상품 목록
+   */
+  List<MyInvestProduct> myInvestList(long userId);
+}
