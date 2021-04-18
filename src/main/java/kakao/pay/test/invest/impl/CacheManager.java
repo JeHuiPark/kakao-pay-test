bc1fa93fb81a70fbda0718c52405995f84fd89f9
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-19 01:00:52 +0900

    자바독을 작성하라
    * CacheManager 자바독을 작성합니다

--- src/main/java/kakao/pay/test/invest/impl/CacheManager.java
+++ src/main/java/kakao/pay/test/invest/impl/CacheManager.java
@@ -9,9 +9,19 @@ import java.util.Optional;
  */
 public interface CacheManager<V, K extends CacheKeyProvider> {
 
+  /**
+   * 주어진 key 와 일치하는 캐시 데이터를 제거합니다.
+   */
   void purge(K key);
 
+  /**
+   * 캐시 데이터를 key 에 저장합니다. <br>
+   */
   void set(K key, V value);
 
+  /**
+   * 주어진 key 와 일치하는 캐시 데이터를 조회합니다. <br>
+   * 저장된 캐시가 없으면 empty 를 반환합니다.
+   */
   Optional<V> get(K key);
 }
