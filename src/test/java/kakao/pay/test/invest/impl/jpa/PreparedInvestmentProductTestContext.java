a2ca741950f2e299861da0ce1fbaf37f808c48bc
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-18 16:45:48 +0900

    테스트 도구를 추가하라
    * 테스트용 투자상품을 준비시키는 클래스를 정의합니다

---
+++ src/test/java/kakao/pay/test/invest/impl/jpa/PreparedInvestmentProductTestContext.java
@@ -0,0 +1,34 @@
+package kakao.pay.test.invest.impl.jpa;
+
+import kakao.pay.test.invest.interfaces.InvestPeriod;
+import kakao.pay.test.invest.interfaces.InvestProductType;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/**
+ * 테스트용 투자상품을 준비시킵니다.
+ */
+@DataJpaTest
+abstract class PreparedInvestmentProductTestContext {
+
+  @Autowired
+  InvestmentProductRepository investmentProductRepository;
+
+  InvestmentProduct preparedProduct;
+
+  @BeforeEach
+  void _setupContext() {
+    preparedProduct = InvestmentProduct.builder()
+        .investPeriod(preparedProductPeriod())
+        .investProductType(InvestProductType.CREDIT)
+        .totalInvestingAmount(preparedProductTotalInvestingAmount())
+        .title("테스트 상품")
+        .build();
+    investmentProductRepository.saveAndFlush(preparedProduct);
+  }
+
+  abstract InvestPeriod preparedProductPeriod();
+
+  abstract long preparedProductTotalInvestingAmount();
+}
