56451f74e113b4335b3b853d933785afb51effe1
kurly-jehuipark <jehui.park@kurlycorp.com>
2021-04-19 09:05:16 +0900

    샘플 데이터를 추가하라

---
+++ sample-data.sql
@@ -0,0 +1,89 @@
+insert into invest_product
+  (title,
+   total_investing_amount,
+   product_type,
+   accumulated_investing_amount,
+   investor_count,
+   started_at,
+   finished_at)
+value (
+    'SAMPLE01',
+    100000000,
+    'CREDIT',
+    0,
+    0,
+    '2021-01-01 00:00:00',
+    '2021-04-20 00:00:00'
+);
+
+insert into invest_product
+(title,
+ total_investing_amount,
+ product_type,
+ accumulated_investing_amount,
+ investor_count,
+ started_at,
+ finished_at)
+    value (
+    'SAMPLE02',
+    100000000,
+    'CREDIT',
+    0,
+    0,
+    '2021-01-01 00:00:00',
+    '2021-04-22 00:00:00'
+);
+
+insert into invest_product
+(title,
+ total_investing_amount,
+ product_type,
+ accumulated_investing_amount,
+ investor_count,
+ started_at,
+ finished_at)
+    value (
+    'SAMPLE03',
+    100000000,
+    'CREDIT',
+    0,
+    0,
+    '2021-01-01 00:00:00',
+    '2021-04-24 00:00:00'
+);
+
+insert into invest_product
+(title,
+ total_investing_amount,
+ product_type,
+ accumulated_investing_amount,
+ investor_count,
+ started_at,
+ finished_at)
+    value (
+    'SAMPLE04',
+    100000000,
+    'CREDIT',
+    0,
+    0,
+    '2021-01-01 00:00:00',
+    '2021-04-26 00:00:00'
+);
+
+insert into invest_product
+(title,
+ total_investing_amount,
+ product_type,
+ accumulated_investing_amount,
+ investor_count,
+ started_at,
+ finished_at)
+    value (
+    'SAMPLE06',
+    100000000,
+    'CREDIT',
+    0,
+    0,
+    '2021-01-01 00:00:00',
+    '2021-05-01 00:00:00'
+);
